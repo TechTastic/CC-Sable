6a61ce6e5bdfba5b5f743b32152c1b87478c0eb4
TechTastic <74630543+TechTastic@users.noreply.github.com>
2026-03-02 10:33:07 -0600

    LDoc shenanigans

--- common/src/main/resources/data/computercraft/lua/rom/apis/sublevel.lua
+++ common/src/main/resources/data/computercraft/lua/rom/apis/sublevel.lua
@@ -1,3 +1,40 @@
+--- This API is added by CC: Sable and allows CC: Tweaked computers to access information from Sable Sub-Levels.
+--
+-- This library also includes [CC: Advanced Math][advanced-math] which provides the `quaternion` API.
+--
+-- [advanced-math]: https://techtastic.github.io/Advanced-Math/
+--
+-- @module sublevel
+
+--- Determines whether the computer is currently on a Sub-Level.
+-- @function isInPlotGrid
+-- @treturn boolean `true` if the computer is on a Sub-Level, `false` otherwise
+
+--- Gets the Sub-Level's Universally Unique Identifier (UUID).
+-- @function getUniqueId
+-- @treturn string The Sub-Level UUID
+-- @raise This method errors if there is no Sub-Level associated with the computer.
+
+--- Gets the Sub-Level's name.
+-- @function getName
+-- @treturn string The Sub-Level name
+-- @raise This method errors if there is no Sub-Level associated with the computer.
+
+--- Gets the Sub-Level's logical pose of the Sub-Level.
+-- @function getLogicalPose
+-- @treturn table Pose information including position, orientation, scale, and rotation point
+-- @raise This method errors if there is no Sub-Level associated with the computer.
+
+--- Gets the Sub-Level's last pose of the Sub-Level.
+-- @function getLastPose
+-- @treturn table Pose information including position, orientation, scale, and rotation point
+-- @raise This method errors if there is no Sub-Level associated with the computer.
+
+--- Gets the Sub-Level's global velocity.
+-- @function getVelocity
+-- @treturn vector the global velocity of the Sub-Level
+-- @raise This method errors if there is no Sub-Level associated with the computer.
+
 if not sublevel then
     error("Cannot load Sub-Level API on computer")
 end
